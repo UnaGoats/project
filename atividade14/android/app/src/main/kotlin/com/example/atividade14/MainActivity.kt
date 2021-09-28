2b0435cab5b82e3801fc09207488623937a52966
Pablo Santos <brdeveloperday@gmail.com>
2021-09-28 20:59:08 -0300

    atividade14 GO!

---
+++ atividade14/android/app/src/main/kotlin/com/example/atividade14/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade14
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ atividade14/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'route/route.dart';
+
+void main() => runApp(
+      MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => PrimeiraTela(),
+          '/segunda': (context) => SegundaTela(),
+          '/terceira': (context) => TerceiraTela(),
+        },
+      ),
+    );
