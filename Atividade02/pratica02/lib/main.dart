46d9d6671fcba6ae300467093791cc5a0684ec1c
Brant <viniassis_brantcouto@hotmail.com>
2021-11-09 18:26:57 -0300

    Atividade 02

---
+++ Atividade02/pratica02/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Bom dia, Meu nome é Vinicius Brant',
+        textDirection: TextDirection.ltr,
+      ), //Text.
+    ), //Center.
+  );
+}
